4f47b8a89ac047db774c67384133360ce2c9f3d7
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-01-04 16:34:45 +0000

    Added contacts list component

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 angular.module('contacts', ['ngMaterial'])
 	.component('app', {
-		templateUrl: 'src/app.html'
+		templateUrl: 'src/app.html',
+		controller: function() {
+			this.contacts = [
+				{name: 'Chip Smith', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'},
+				{name: 'Randy Horn', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'},
+				{name: 'Zane High', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'}
+			];
+		}
+	})
+	.component('contactList', {
+		templateUrl: 'src/contactList.html',
+		bindings: {
+			contacts: '<'
+		}
 	});

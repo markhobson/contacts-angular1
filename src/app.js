f4dbb4e2b233fc01896883ce7355deffb704d614
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-01-05 16:32:45 +0000

    Allow contacts to be edited

--- src/app.js
+++ src/app.js
@@ -2,18 +2,26 @@ angular.module('contacts', ['ngMaterial'])
 	.factory('contactRepository', function() {
 		return {
 			contacts: [
-				{name: 'Chip Smith', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'},
-				{name: 'Randy Horn', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'},
-				{name: 'Zane High', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'}
-			]
+				{id: '0', name: 'Chip Smith', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'},
+				{id: '1', name: 'Randy Horn', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'},
+				{id: '2', name: 'Zane High', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'}
+			],
+			save: function(contact) {
+				this.contacts[contact.id] = contact;
+			}
 		};
 	})
 	.component('app', {
 		templateUrl: 'src/app.html',
-		controller: ['contactRepository', function(contactRepository) {
+		controller: ['contactRepository', '$mdToast', function(contactRepository, $mdToast) {
 			this.contacts = contactRepository.contacts;
 			this.select = function(contact) {
 				this.selectedContact = contact;
+			};
+			this.save = function(contact) {
+				contactRepository.save(contact);
+				this.select(contact);
+				$mdToast.showSimple('Contact saved');
 			}
 		}]
 	})
@@ -36,13 +44,24 @@ angular.module('contacts', ['ngMaterial'])
 	.component('contactForm', {
 		templateUrl: 'src/contactForm.html',
 		controller: function() {
-			this.$onChanges = function(changes) {
-				this.editableContact = {
-					name: this.contact.name
+			var ctrl = this;
+			var edit = function(contact) {
+				ctrl.editableContact = {
+					id: contact.id,
+					name: contact.name,
+					avatar: contact.avatar
 				};
+			};
+			this.$onChanges = function() {
+				edit(this.contact);
+			};
+			this.save = function() {
+				this.onSave({contact: this.editableContact});
+				edit(this.editableContact);
 			};
 		},
 		bindings: {
-			contact: '<'
+			contact: '<',
+			onSave: '&'
 		}
 	});

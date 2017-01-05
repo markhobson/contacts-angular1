6db84666ff02763449bb999108130c414029a005
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-01-05 11:38:32 +0000

    Extracted contacts into contact repository service

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 angular.module('contacts', ['ngMaterial'])
-	.component('app', {
-		templateUrl: 'src/app.html',
-		controller: function() {
-			this.contacts = [
+	.factory('contactRepository', function() {
+		return {
+			contacts: [
 				{name: 'Chip Smith', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'},
 				{name: 'Randy Horn', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'},
 				{name: 'Zane High', avatar: 'https://www.gravatar.com/avatar/ed8fc01d29a00912ba41555210cd697e'}
-			];
+			]
+		};
+	})
+	.component('app', {
+		templateUrl: 'src/app.html',
+		controller: ['contactRepository', function(contactRepository) {
+			this.contacts = contactRepository.contacts;
 			this.select = function(contact) {
 				this.selectedContact = contact;
 			}
-		}
+		}]
 	})
 	.component('contactList', {
 		templateUrl: 'src/contactList.html',

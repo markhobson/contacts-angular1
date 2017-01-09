3a458ed888aec79e3785b8f8867e6acad43365d5
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-01-09 14:33:02 +0000

    Switched to ui-router

--- src/contacts.js
+++ src/contacts.js
@@ -1,12 +1,18 @@
-angular.module('contacts', ['ngMaterial', 'ngMessages', 'ngRoute'])
-	.config(function($routeProvider) {
-		$routeProvider
-			.when('/', {
+angular.module('contacts', ['ngMaterial', 'ngMessages', 'ui.router'])
+	.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+		$urlRouterProvider.when('', '/');
+		
+		$stateProvider
+			.state({
+				name: 'main',
+				url: '/',
 				template: '<contact-manager></contact-manager>'
 			})
-			.when('/contact/:contactId', {
-				template: function(routeParams) {
-					return '<contact-manager contact-id="' + routeParams.contactId + '"></contact-manager>';
+			.state({
+				name: 'contact',
+				url: '/contact/:contactId',
+				template: function(stateParams) {
+					return '<contact-manager contact-id="' + stateParams.contactId + '"></contact-manager>';
 				}
 			});
-	});
+	}]);

becf2459144b755cc9ea219e593033f241f59468
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-01-10 16:35:13 +0000

    Consolidated component initialisation

--- src/contactManager.js
+++ src/contactManager.js
@@ -2,10 +2,14 @@ angular.module('contacts')
 	.component('contactManager', {
 		templateUrl: 'src/contactManager.html',
 		controller: ['contactRepository', '$mdToast', '$scope', '$state', function(contactRepository, $mdToast, $scope, $state) {
-			var ctrl = this;
-			$scope.$watch(contactRepository.getAll, function(newValue) {
-				ctrl.contacts = newValue;
-			}, true);
+			this.$onInit = function() {
+				var ctrl = this;
+				$scope.$watch(contactRepository.getAll, function(newValue) {
+					ctrl.contacts = newValue;
+				}, true);
+				var contact = contactRepository.get(this.contactId);
+				this.selectedContact = contact;
+			};
 			this.select = function(contact) {
 				$state.go('contact', {contactId: contact.id});
 			};
@@ -23,10 +27,6 @@ angular.module('contacts')
 				contactRepository.delete(contact);
 				this.select();
 				$mdToast.showSimple((contact.name || 'Unnamed') + ' deleted');
-			};
-			this.$onInit = function() {
-				var contact = contactRepository.get(this.contactId);
-				this.selectedContact = contact;
 			};
 		}],
 		bindings: {
